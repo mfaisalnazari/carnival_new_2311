e05a1cfee1f04e0e5c3bccad515fc3c106b52189
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2024-01-09 09:37:38 -0700

    visitor_spec_file_finished

---
+++ lib/visitor.rb
@@ -0,0 +1,17 @@
+class Visitor
+    attr_reader :name, :height, :spending_money, :preferences
+    def initialize(name, height, spending_money)
+        @name = name
+        @height = height
+        @spending_money = spending_money
+        @preferences = []
+    end
+
+    def add_preference(preference)
+        @preferences << preference
+    end
+    
+    def tall_enough?(required_height)
+        @height >= required_height
+    end
+end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,2 +1,26 @@
 require 'spec_helper'
 
+RSpec.describe Visitor do
+    let(:visitor1) {Visitor.new("Bruce", 54 , "$10")}
+    it 'exists' do
+        expect(visitor1).to be_instance_of(Visitor)
+    end
+    it 'has attributes' do 
+        expect(visitor1.name).to eq("Bruce")
+        expect(visitor1.height).to eq(54)
+        expect(visitor1.spending_money).to eq("$10")
+        expect(visitor1.preferences).to eq([])
+    end
+    it 'methods work' do
+        visitor2 = Visitor.new("tucker", 36, "$5")
+        visitor3 = Visitor.new("Penny", 64, "$15")
+        visitor1.add_preference(:gentle)
+        visitor1.add_preference(:thrilling)
+        expect(visitor1.preferences).to eq([:gentle, :thrilling])
+        expect(visitor1.tall_enough?(54)).to be(true)
+        expect(visitor2.tall_enough?(54)).to be(false)
+        expect(visitor3.tall_enough?(54)).to be(true)
+        
+    end
+
+end
