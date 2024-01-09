3be4908b07e7a51cdc67c3b6ceee0a543cd3899e
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2024-01-09 11:20:38 -0700

    total_revenue_done

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -1,8 +1,19 @@
 class Carnival
-    attr_reader :duration, :rides, :total_revenue
+    attr_reader :duration, :rides
     def initialize(duration)
         @duration = duration
         @rides = []
-        @total_revenue = 0
+    end
+    
+    def add_ride(ride)
+        rides << ride
+    end
+
+    def total_revenue
+        total_revenue = 0
+        @rides.each do |ride|
+            total_revenue += ride.total_revenue
+        end
+        total_revenue
     end
 end
