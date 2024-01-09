38b557ebeba88fec73e813befca268858e620b05
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2024-01-09 11:58:50 -0700

    carnival_done

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -16,4 +16,30 @@ class Carnival
         end
         total_revenue
     end
+
+    def most_popular_ride
+        rides_ride_log = []
+        
+        @rides.each do |ride|
+            rides_ride_log << ride.rider_log.values.sum
+        end
+        popular_ride_number = rides_ride_log.max
+        popular_ride = @rides.select {|ride| ride.rider_log.values.sum == popular_ride_number}
+        most_popular_ride = popular_ride[0]
+        most_popular_ride
+        
+    end
+
+    def most_profitable_ride
+        rides_revenue = []
+        
+        @rides.each do |ride|
+           rides_revenue << ride.total_revenue
+        end
+        max_revenue = rides_revenue.max
+        revenue_ride = @rides.select {|ride| ride.total_revenue == max_revenue}
+        max_revenue_ride = revenue_ride[0]
+        max_revenue_ride
+        
+    end
 end
